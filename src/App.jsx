ea1169814fab53a035004c575e22253be899b701
Brijesh Kumar <iconbrijesh@gmail.com>
2025-05-30 23:12:25 +0530

    added cdn links of bootstrap and fa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Hero from "./landing_page/Home/Hero.jsx";
 
 function App() {
   
   return (
     <>
       <h1>my name is brijesh kumar</h1>
+      <Hero />
       
     </>
   )
---
+++ src/landing_page/Home/Hero.jsx
@@ -0,0 +1,15 @@
+
+function Hero() {
+    return (
+        <div>
+            <button className="btn btn-primary">
+                Click Me!
+            </button>
+            <i className="fa-solid fa-house"></i>
+            <i class="fa-solid fa-truck"></i>
+        </div>
+
+    );
+}
+
+export default Hero;
